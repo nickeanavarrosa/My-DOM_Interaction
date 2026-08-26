40e5aa2fa7ae78372fc9bfc48ef7350d6ab84055
nickeanavarrosa <nickeanavarrosa@gmail.com>
2026-08-26 08:56:05 +0800

    Add score counter functionality in app.js

---
+++ scenario 1/app.js
@@ -0,0 +1,16 @@
+app.js
+let score = 0;
+
+const countDisplay = document.getElementById("count");
+const incrementButton = document.getElementById("incrementBtn");
+const resetButton = document.getElementById("resetBtn");
+
+incrementButton.addEventListener("click", function () {
+score++;
+countDisplay.innerHTML = score;
+});
+
+resetButton.addEventListener("click", function () {
+score = 0;
+countDisplay.innerHTML = score;
+});
